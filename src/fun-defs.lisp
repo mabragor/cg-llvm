80e78291890926087ee6cff845dd89195cc99b2e
Alexandr Popolitov <popolit@gmail.com>
2016-01-22 15:23:58 +0100

    First attempt on most constexprs

--- src/fun-defs.lisp
+++ src/fun-defs.lisp
@@ -772,6 +772,7 @@
 						  (list (intern #?"$((string ,name-var))-INSTRUCTION")
 							,name-var)
 						  (list (car ,name-var) (cadr ,name-var)))
+     (declare (ignorable rule-name instr-name))
      ,@body))
   
 (defmacro!! define-instruction-rule (name &body body) ()
@@ -816,9 +817,17 @@
 		   kwd-var))))
 
 (defmacro define-instruction-alternative (name &body alternatives)
-  (let ((rule-name (append-instruction-to-sym name))
-	(alt-names (mapcar #'append-instruction-to-sym alternatives)))
-    `(define-cg-llvm-rule ,rule-name () (|| ,@alt-names))))
+  (if (symbolp name)
+      (let ((rule-name (append-instruction-to-sym name))
+	    (alt-names (mapcar #'append-instruction-to-sym alternatives)))
+	`(define-cg-llvm-rule ,rule-name () (|| ,@alt-names)))
+      (destructuring-bind (name appendix) name
+	(let ((rule-name (mash-sym-names name appendix))
+	      (alt-names (mapcar #'(lambda (x)
+				     (mash-sym-names x appendix))
+				 alternatives)))
+	  `(define-cg-llvm-rule ,rule-name () (|| ,@alt-names))))))
+	
 
 (defmacro define-lvalue-instruction-alternative (name)
   `(define-instruction-alternative ,name ,(mash-sym-names 'lvalue name) ,(mash-sym-names 'nolvalue name)))
@@ -954,27 +963,50 @@
 	    white-comma
 	    (let ((arg2 (parse-arg)))
 	      `(,type ,arg1 ,arg2 ,@prefix-kwds)
-	      )))))))
+	      ))))))
+  (defun binop-constexpr-body (name type)
+    `((let* ((val1 (progn wh? #\( wh? llvm-constant))
+	     (val2 (prog1 (progn wh? #\, wh? llvm-constant) wh? #\))))
+	(if (not (or (llvm-typep '(,type ***) (car val1))
+		     (llvm-typep '(vector (,type ***) *) (car val1))))
+	    (fail-parse-format ,#?"$((string name)) constexpr expects <type> to be $((string type)) \
+                                      or VECTOR OF $((string type))S, but got: ~a"
+			       (car val1)))
+	(if (not (llvm-same-typep (car val1) (car val2)))
+	    (fail-parse-format ,#?"$((string name)) constexpr expects types of its arguments to coincide \
+                                      but got: ~a and ~a"
+			       (car val1) (car val2)))
+	`(,val1 ,val2 ,@prefix-kwds)))))
+  
 
 (defmacro define-integer-binop-definer (name &optional prefix)
-  `(defmacro ,name (name)
-     (with-rule-names (name)
-       `(define-instruction-rule ,name
-	  (,,(if prefix
-		 (intern #?"WITH-$((string prefix))-PREFIX")
-		 'progn)
-	     ,@(binop-instruction-body instr-name 'integer))))))
-
+  (let ((constexpr-name (intern #?"$((string name))-CONSTEXPR")))
+    `(defmacro ,name (name)
+       (with-rule-names (name)
+	 `(progn (define-instruction-rule ,name
+		   (,,(if prefix
+			  (intern #?"WITH-$((string prefix))-PREFIX")
+			  'progn)
+		      ,@(binop-instruction-body instr-name 'integer)))
+		 (define-instruction-rule (,,constexpr-name ,instr-name)
+		   (,,(if prefix
+			  (intern #?"WITH-$((string prefix))-PREFIX")
+			  'progn)
+		      ,@(binop-constexpr-body instr-name 'integer))))))))
 
 (define-integer-binop-definer define-integer-binop-rule integer-overflow)
 (define-integer-binop-definer define-exact-integer-binop-rule exact)
 (define-integer-binop-definer define-simple-integer-binop-rule)
 
 (defmacro define-float-binop-rule (name &key (type 'float))
-  (with-rule-names (name)
-    `(define-instruction-rule ,name
-       (with-fast-math-flags-prefix
-	 ,@(binop-instruction-body instr-name type)))))
+  (let ((constexpr-name (intern (format nil #?"~a-CONSTEXPR" name))))
+    (with-rule-names (name)
+      `(progn (define-instruction-rule ,name
+		(with-fast-math-flags-prefix
+		  ,@(binop-instruction-body instr-name type)))
+	      (define-instruction-rule (,constexpr-name ,instr-name)
+		(with-fast-math-flags-prefix
+		  ,@(binop-instruction-body instr-name type)))))))
 
 (define-integer-binop-rule add)
 (define-integer-binop-rule sub)
@@ -1637,5 +1669,130 @@
 (define-cast-constexpr bitcast (bitcast-constraints))
 (define-cast-constexpr addrspacecast (pointer->pointer-based have-different-addrspaces))
 
+(define-instruction-alternative (cast to-constexpr)
+  trunc zext sext fptrunc fpext fptoui fptosi uitofp sitofp ptrtoint inttoptr
+  bitcast addrspacecast)
+
+(define-instruction-rule (select-constexpr select)
+  (let* ((cond (?wh (progn #\( wh? (let ((it llvm-constant))
+				     (if (not (or (llvm-typep '(integer 1) (car it))
+						  (llvm-typep '(vector (integer 1) *) (car it))))
+					 (fail-parse "SELECT constexpr 'cond' argument should be byte(???)")
+					 it)))))
+	 (val1 (progn white-comma llvm-constant))
+	 (val2 (progn white-comma llvm-constant)))
+    (if (llvm-typep '(integer *) (car cond))
+	(if (not (llvm-same-typep (car val1) (car val2)))
+	    (fail-parse-format "Different values of SELECT constexpr should be same type, but are: ~a ~a"
+			       (car val1) (car val2)))
+	(let ((nelts (caddar cond)))
+	  (if (not (and (llvm-typep '(vector ***) (car val1))
+			(llvm-typep '(vector ***) (car val2))
+			(llvm-same-typep (cadar val1) (cadar val2))
+			(equal nelts (caddar val1))
+			(equal nelts (caddar val2))))
+	      (fail-parse-format "Different values of SELECT constexpr should be same type, but are: ~a ~a"
+				 (car val1) (car val2)))))
+    `(,cond ,val1 ,val2)))
+
+
+
+
+(defmacro define-cmp-constexpr (name typecheck &rest errinfo)
+  (let ((macro-name (intern #?"$((string name))-KWDS"))
+	(constexpr-name (intern #?"$((string name))-CONSTEXPR")))
+    `(define-instruction-rule (,constexpr-name ,name)
+       (let* ((cond (wh (,macro-name)))
+	      (val1 (progn wh? #\( wh? llvm-constant))
+	      (val2 (prog1 (progn white-comma llvm-constant) wh? #\))))
+	 (if (not ,typecheck)
+	     (fail-parse-format ,@errinfo))
+	 `(,cond ,val1 ,val2)))))
+
+(define-cmp-constexpr icmp
+    (or (llvm-typep '(integer ***) (car val1))
+	(llvm-typep '(vector (integer ***) *) (car val1))
+	(llvm-typep '(pointer ***) (car val1))
+	(llvm-typep '(vector (pointer ***) *) (car val1)))
+  "Type of arguments of ICMP constexpr should be INTEGER or POINTER or~
+    VECTOR of INTEGERS or VECTOR of POINTERS")
+
+(define-cmp-constexpr fcmp
+    (or (llvm-typep '(float ***) (car val1))
+	(llvm-typep '(vector (float ***) *) (car val1)))
+  "Type of arguments of FCMP constexpr should be FLOAT or~
+    VECTOR of FLOATs")
+
+(define-instruction-rule (insertvalue-constexpr insertvalue)
+  (let ((val (progn wh? #\( wh? llvm-constant)))
+    (if (not (or (llvm-typep '(struct ***) (car val))
+		 (llvm-typep '(array ***) (car val))))
+	(fail-parse-format "Type of INSERTVALUE constexpr should be aggregate, but got ~a" (car val)))
+    white-comma
+    (let* ((elt llvm-constant)
+	   (indices (prog1 indices wh? #\))))
+      ;; TODO : check that elt has same type as elt of val
+      `(,val ,elt ,@indices))))
+
+(define-instruction-rule (extractvalue-constexpr extractvalue)
+  (let ((val (progn wh? #\( wh? llvm-constant)))
+    (if (not (or (llvm-typep '(struct ***) (car val))
+		 (llvm-typep '(array ***) (car val))))
+	(fail-parse-format "Type of EXTRACTVALUE constexpr should be aggregate, but got ~a" (car val)))
+    white-comma
+    (let* ((indices (prog1 indices wh? #\))))
+      ;; TODO : check that elt has same type as elt of val
+      `(,val ,@indices))))
+
+(define-instruction-rule (shufflevector-constexpr shufflevector)
+  (let* ((v1 (progn wh? #\( wh? llvm-constant))
+	 (v2 (progn white-comma llvm-constant))
+	 (mask (prog1 (progn white-comma llvm-constant) wh? #\))))
+    (if (not (and (llvm-typep '(vector ***) (car v1))
+		  (llvm-typep '(vector ***) (car v2))
+		  (llvm-typep '(vector (integer 32) *) (car mask))))
+	(fail-parse "Shuffle vector accepts two vectors and one i32 mask"))
+    (if (not (llvm-same-typep (cadar v1) (cadar v2)))
+	(fail-parse "V1 and V2 must have same subtype"))
+    `(,v1 ,v2 ,mask)))
+
+(define-instruction-rule (extractelement-constexpr extractelement)
+  (let* ((val (progn wh? #\( wh? llvm-constant))
+	(idx (prog1 (progn white-comma llvm-constant) wh? #\))))
+    (if (not (llvm-typep '(vector ***) (car val)))
+	(fail-parse-format "EXTRACTELEMENT constexpr 1st arg is vector type, but got: ~a" (car val)))
+    (if (not (llvm-typep '(integer ***) (car idx)))
+	(fail-parse-format "EXTRACTELEMENT constexpr 2nd arg is integer type, but got: ~a" (car idx)))
+    `(,val ,idx)))
+
+(define-instruction-rule (insertelement-constexpr insertelement)
+  (let* ((val (progn wh? #\( wh? llvm-constant))
+	 (elt (progn white-comma llvm-constant))
+	 (idx (prog1 (progn white-comma llvm-constant) wh? #\))))
+    (if (not (llvm-typep '(vector ***) (car val)))
+	(fail-parse-format "INSERTELEMENT constexpr 1st arg is vector type, but got: ~a" (car val)))
+    (if (not (llvm-typep '(integer ***) (car idx)))
+	(fail-parse-format "INSERTELEMENT constexpr 3rd arg is integer type, but got: ~a" (car idx)))
+    (if (not (llvm-same-typep (car elt) (cadar val)))
+	(fail-parse "ELT must be same type as subtype of VAL"))
+    `(,val ,elt ,idx)))
+
+
+(define-instruction-rule (getelementptr-constexpr getelementptr)
+  (let* ((inbounds (?wh (progn "inbounds" t)))
+	 (type (progn wh? #\( wh? llvm-type))
+	 (indices indices))
+    `(,type ,@indices ,!m(inject-kwd-if-nonnil inbounds))))
+
+
+(define-instruction-alternative (special constexpr)
+  getelementptr select icmp fcmp extractelement insertelement shufflevector
+  extractvalue insertvalue opcode)
+
+(define-instruction-alternative (opcode constexpr)
+  add sub mul fadd fsub fmul frem fdiv udiv sdiv urem srem
+  shl lshr ashr and or xor)
+
 (define-cg-llvm-rule constant-expression ()
-  ...)
+  (|| cast-to-constexpr
+      special-constexpr))
