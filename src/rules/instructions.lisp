71df152837aaf11fb31687bb382abfe7e4e5a1dc
terminal625 <zpersoncool@gmail.com>
2018-08-26 04:45:45 -0400

    nest binop-constexpr-body

--- src/rules/instructions.lisp
+++ src/rules/instructions.lisp
@@ -256,33 +256,48 @@
 	 (let- (arg2 (parse-arg)))
 	 `(,type ,arg1 ,arg2 ,@prefix-kwds)))))
   (defun binop-constexpr-body (name type)
-    `((let* ((val1 (progn-v wh? #\( wh? llvm-constant))
-	     (val2 (prog1-v (progn-v wh? #\, wh? llvm-constant) wh? #\))))
-	(if (not (or (llvm-typep '(,type ***) (car val1))
-		     (llvm-typep '(vector (,type ***) *) (car val1))))
-	    (fail-parse-format
-	     ,(progn
+    (let ((fail-format
+	   (progn
 	       ;;;$((string name)) constexpr expects <type> to be $((string type))
-	       ;;	       or VECTOR OF $((string type))S, but got: ~a
-	       (interpol
-		(string name)
-		" constexpr expects <type> to be "
-		(string type)
-		"~% or VECTOR OF"
-		(string type)
-		"S, but got: ~a"))
-	     (car val1)))
-	(if (not (llvm-same-typep (car val1) (car val2)))
-	    (fail-parse-format
-	     ,(progn
-	       ;;$((string name)) constexpr expects types of its arguments to coincide
-	       ;;but got: ~a and ~a
-	       (interpol
-		(string name)
-		" constexpr expects types of its arguments to coincide~% but got: ~a and ~a"))
-	     (car val1)
-	     (car val2)))
-	`(,val1 ,val2 ,@prefix-kwds)))))
+	     ;;	       or VECTOR OF $((string type))S, but got: ~a
+	     (interpol
+	      (string name)
+	      " constexpr expects <type> to be "
+	      (string type)
+	      "~% or VECTOR OF"
+	      (string type)
+	      "S, but got: ~a")))
+	  (fail2
+	   (progn
+	     ;;$((string name)) constexpr expects types of its arguments to coincide
+	     ;;but got: ~a and ~a
+	     (interpol
+	      (string name)
+	      " constexpr expects types of its arguments to coincide~% but got: ~a and ~a"))))
+      `((nest
+	 (progn (v wh?))
+	 (progn (v #\())
+	 (progn (v wh?))
+	 (let- (val1 (v llvm-constant)))
+	 (progn (v wh?))
+	 (progn (v #\,))
+	 (progn (v wh?)) 
+	 (let- (val2 (v llvm-constant)))
+	 (progn (v wh?))
+	 (progn (v #\)))
+	 (progn
+	   (if (not (or (llvm-typep '(,type ***) (car val1))
+			(llvm-typep '(vector (,type ***) *) (car val1))))
+	       (fail-parse-format
+		,fail-format
+		(car val1))))
+	 (progn
+	   (if (not (llvm-same-typep (car val1) (car val2)))
+	       (fail-parse-format
+		,fail2
+		(car val1)
+		(car val2))))
+	 `(,val1 ,val2 ,@prefix-kwds))))))
 
 (defmacro unordered-simple-keywords (&rest kwds)
   `(let ((kwds (times (wh (|| ,@(mapcar (lambda (x)
